6f5d8c09d08844bff9dfb02cc3bf9b364b7f0b8e
Harry Chen <i@harrychen.xyz>
2019-07-21 23:46:19 +0800

    Port to TrivialMIPS NSCSCC
    Signed-off-by: Harry Chen <i@harrychen.xyz>

---
+++ board/nscscc/2019/trivialmips_nscscc_2019.c
@@ -0,0 +1,43 @@
+
+#include <common.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+/* initialize the DDR Controller and PHY */
+int dram_init(void)
+{
+    /* MIG IP block is smart and doesn't need SW
+     * to do any init */
+    gd->ram_size = CONFIG_SYS_SDRAM_SIZE;   /* in bytes */
+    return 0;
+}
+
+#ifdef CONFIG_RESET_PHY_R
+void reset_phy(void) {}
+#endif
+
+
+uint64_t notrace get_ticks(void)
+{
+	return readl((volatile uint32_t*) TRIVIALMIPS_NSCSCC_TIMER_ADDR);
+}
+
+ulong get_timer(ulong base)
+{
+        return timer_get_us() / 1000 - base;
+}
+
+unsigned long notrace timer_get_us(void)
+{
+        return get_ticks() / 100;
+}
+
+void __udelay(unsigned long usec)
+{
+        uint64_t tmp;
+
+        tmp = timer_get_us() + usec; /* get current timestamp */
+
+        while (timer_get_us() < tmp+1)     /* loop till event */
+                 /*NOP*/;
+}
---
+++ include/configs/trivialmips_nscscc.h
@@ -0,0 +1,90 @@
+#ifndef __TRIVIALMIPS_NSCSCC_CONFIG_H
+#define __TRIVIALMIPS_NSCSCC_CONFIG_H
+
+/* BootROM + MIG is pretty smart. DDR and Cache initialized */
+#define CONFIG_SKIP_LOWLEVEL_INIT
+
+#define DEBUG
+
+/*--------------------------------------------
+ * CPU configuration
+ */
+/* CPU Timer rate */
+#define CONFIG_SYS_MIPS_TIMER_FREQ	100000000
+
+/* Cache Configuration */
+#define CONFIG_SYS_MIPS_CACHE_MODE	CONF_CM_CACHABLE_NONCOHERENT
+
+/* Time Measuring */
+#define TRIVIALMIPS_NSCSCC_TIMER_ADDR		0xbff0e000
+
+/*----------------------------------------------------------------------
+ * Memory Layout
+ */
+
+/* SDRAM Configuration (for final code, data, stack, heap) */
+#define CONFIG_SYS_SDRAM_BASE		0x80000000
+#define CONFIG_SYS_SDRAM_SIZE		0x08000000	/* 128 Mbytes */
+#define CONFIG_SYS_INIT_SP_ADDR		\
+	(CONFIG_SYS_SDRAM_BASE + CONFIG_SYS_SDRAM_SIZE - 0x1000)
+
+#define CONFIG_SYS_MEMTEST_START    0x80000000
+#define CONFIG_SYS_MEMTEST_END      0x08000000
+
+#define CONFIG_SYS_MALLOC_LEN		(256 << 10)
+#define CONFIG_SYS_MONITOR_BASE		CONFIG_SYS_TEXT_BASE
+#define CONFIG_SYS_LOAD_ADDR		0x80000000 /* default load address */
+
+/*----------------------------------------------------------------------
+ * Commands
+ */
+//#define CONFIG_SYS_LONGHELP		/* undef to save memory */
+//#define CONFIG_CMDLINE_EDITING
+
+/*------------------------------------------------------------
+ * Console Configuration
+ */
+#define CONFIG_SYS_CBSIZE		1024 /* Console I/O Buffer Size   */
+#define CONFIG_SYS_MAXARGS		16   /* max number of command args*/
+
+/* -------------------------------------------------
+ * Environment
+ */
+//Disable persistent environment variable storage
+#define CONFIG_ENV_IS_NOWHERE   1 
+
+
+#define CONFIG_ENV_SIZE		0x4000
+#define CONFIG_EXTRA_ENV_SETTINGS \
+    "autoload=no\0" \
+    "s_linux=setenv bootfile mips-linux.ub;setenv loadaddr 0x800FFFC0\0" \
+    "s_ucore=setenv bootfile ucore.ub;setenv loadaddr 0x80000000\0" \
+    "serverip=192.168.1.30\0" \
+    "ipaddr=192.168.1.60\0" \
+    "netmask=255.255.255.0\0"
+
+
+
+/* ---------------------------------------------------------------------
+ * Board boot configuration
+ */
+
+#define CONFIG_TIMESTAMP	/* Print image info with timestamp */
+
+
+/* Flash */
+#define CONFIG_FLASH_CFI_DRIVER
+#define CONFIG_SYS_FLASH_CFI
+#define CONFIG_SYS_FLASH_USE_BUFFER_WRITE
+#define CONFIG_SYS_MAX_FLASH_BANKS_DETECT   1
+#define CONFIG_SYS_MAX_FLASH_SECT           64
+
+
+#define CONFIG_ENV_IS_NOWHERE      1
+#define CONFIG_ENV_SIZE 0x1000
+
+
+// #define DEBUG
+
+#endif /* __TRIVIALMIPS_NSCSCC_CONFIG_H */
+
